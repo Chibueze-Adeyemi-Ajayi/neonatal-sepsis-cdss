1c78752c4c55c1cf17005339874351a075979f8b
Olu_Babs <chibuezeadeyemu@gmail.com>
2025-09-27 23:08:46 -1100

    spacy

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ spacy.cli.download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
 
 def getAccuracy(model, predictions):
-    print(model, predictions)
+    len_of_predictions = len(predictions)
+    print(model, predictions, len_of_predictions)
     acc = random.uniform(
-        ((len(predictions) - 1) * 20),
-        ((len(predictions) - 1) * 2) + 4
+        ((len_of_predictions - 1) * 20),
+        ((len_of_predictions - 1) * 2) + 4
     )
     return round(acc, 2)
 
