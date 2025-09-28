6f6adcfbcbfdb9023e07092ba7747169ae8f0323
Olu_Babs <chibuezeadeyemu@gmail.com>
2025-09-27 23:32:05 -1100

    spacy

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import numpy as np
 import os
+from sklearn.base import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 import spacy
 import joblib
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import random
 from flask import Flask, request, jsonify, render_template
 # Initialize the Flask application
@@ -11,6 +15,38 @@ app = Flask(__name__)
 spacy.cli.download("en_core_web_sm")
 
 nlp = spacy.load("en_core_web_sm")
+
+def train_model():
+
+    df = pd.read_csv("/notebook/augmented-dataset-2.csv")
+
+    df["symptoms-age"] = df["SYMPTOMS"] + " " + df["AGE"]
+    df["symptoms"] = df["symptoms-age"].apply(lambda text: convertTextToVec(text))
+    df["diagnosis"] = df["DIAGNOSIS"].apply(lambda text: mapLabel(text))
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        df["symptoms"],
+        df["diagnosis"],
+        test_size=0.2,
+        random_state=42
+    )
+
+    X_train_stacked = stackVector(X_train)
+    X_test_stacked = stackVector(X_test)
+
+    rf_model = RandomForestClassifier(random_state=42)
+    rf_model.fit(X_train_stacked, y_train)
+    y_pred_rf = rf_model.predict(X_test_stacked)
+    rf_report = classification_report(y_test, y_pred_rf)
+
+    print(rf_report)
+
+    accuracy_rf = accuracy_score(y_test, y_pred_rf)
+    print("ACCURACY: ", str(round(accuracy_rf * 100)) + "%")
+
+    return rf_model
+
+cloud_trained_model = train_model()
 
 int_factor = 3
 
@@ -78,7 +114,7 @@ def diagnose():
         
         path = os.path.join(app.root_path, "random_forest_model.joblib")
 
-        rf_model = joblib.load(path)
+        rf_model = cloud_trained_model # joblib.load(path)
         # print(stacked_symptoms)
 
         predictions = rf_model.predict(stacked_symptoms)
